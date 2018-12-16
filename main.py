7986f2c6c1657aeaf78905460b33fada25fa1b1a
Andrew Dinh <45807756+andrewkdinh@users.noreply.github.com>
2018-12-16 20:11:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,50 @@
 # Description: Get, parse, and interpret JSON files from IEX
 import urllib.request
 import re
-import os
+#import os, errno
+import json
+
+file_path = "tmp/data.txt"
+# directory = os.path.dirname(file_path)
 
 url = urllib.request.urlopen("https://api.iextrading.com/1.0/stock/aapl/chart")
-print("url =", url)
+#print("url =", url)
 html = url.read() # this is not a string, must convert it for findall()
-# print("-----------------------------------------------")
 # print("the html =", html)
 
-if not os.path.exists(temporary):
-    os.makedirs(temporary)
+myFile = open('data.json','r')
+data = myFile.read()
 
-myFile = open('temporary/data.txt','r')
-listOfLines = myFile.readlines()
-print("fileAsList:\n",listOfLines)
+json_string = json.dumps(data)
+print("json string:", data)
 
+'''
+myFile =open(file_path,'w')
+myFile.write(str(html))
+myFile.close()
+
+
+# Create tmp folder
+try:
+    os.makedirs(directory)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+myFile = open(file_path,'r')
+listOfLines = myFile.read().split('}')
+#print("fileAsList:\n",listOfLines)
+
+print(listOfLines[0],"\n")
+print(listOfLines[1])
+'''
+'''
+for j in range(0,len(listOfLines),1):
+  aLine = listOfLines[j]
+  #print(aLine)
+  lineData = aLine.split()
+  #print(lineData)
+'''
 
 '''
 url = urllib.request.urlopen("http://www.gavilan.edu/staff/dir.php")
