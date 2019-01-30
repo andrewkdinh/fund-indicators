765b1f4012f45ec44d7c9c3162891a65f2001cd1
Andrew Dinh <adinh987@gmail.com>
2019-01-30 08:18:34 -0800

    Last commit

--- README.md
+++ README.md
@@ -1,23 +1,17 @@
 # Mutual Fund Indicators
 
 A project to determine indicators of overperforming mutual funds.
-This project is written in Python and will examine market capitalization, persistence, turnover, and expense ratios.
+This project is written in Python 3 and will examine market capitalization, persistence, turnover, and expense ratios.
 
 ### Prerequisites
 
 `$ pip install -r requirements.txt`
 
-or
-
-`$ pip install requests`
-
-`$ pip install numpy`
-
 ### Quickstart
 
 To begin, run
 
-`$ python(3) main.py`
+`$ python main.py`
 
 Some ticker values to try:
 SPY, VFINX, AAPL, GOOGL
--- main.py
+++ main.py
@@ -16,40 +16,83 @@ FIRST TESTING WITH EXPENSE RATIO
 '''
 
 from StockData import StockData
+from StockReturn import Return
 
-listOfStocks = []
-numberOfStocks = int(input("How many stocks or mutual funds would you like to analyze? "))
+listOfStocksData = []
+listOfStocksReturn = []
+#numberOfStocks = int(input("How many stocks or mutual funds would you like to analyze? ")) # CHANGE BACK LATER
+numberOfStocks = 1
 for i in range(0, numberOfStocks, 1):
   	print("Stock", i+1, ": ", end='')
   	stockName = str(input())
-  	listOfStocks.append(i)
-  	listOfStocks[i] = StockData()
-  	listOfStocks[i].setName(stockName)
-  	#print(listOfStocks[i].name)
+  	listOfStocksData.append(i)
+  	listOfStocksData[i] = StockData()
+  	listOfStocksData[i].setName(stockName)
+	# print(listOfStocksData[i].name)
+
+	#listOfStocksReturn.append(i)
+	#listOfStocksReturn[i] = StockReturn()
+
+
+# Decide on a benchmark
+benchmarkTicker = ''
+while benchmarkTicker == '':
+	listOfBenchmarks = ['S&P500', 'DJIA', 'Russell 3000', 'MSCI EAFE']
+	listOfBenchmarksTicker = ['SPY', 'DJIA', 'VTHR', 'EFT']
+	print('\nList of benchmarks:', listOfBenchmarks)
+	#benchmark = str(input('Benchmark to compare to: '))
+	benchmark = 'S&P500'
+
+	for i in range(0,len(listOfBenchmarks), 1):
+		if benchmark == listOfBenchmarks[i]:
+			benchmarkTicker = listOfBenchmarksTicker[i]
+			i = len(listOfBenchmarks)
+
+	if benchmarkTicker == '':
+		print('Benchmark not found. Please type in a benchmark from the list')
+
+print('\n', benchmark, ' (', benchmarkTicker, ')', sep='')
+
+benchmarkName = str(benchmark)
+benchmark = StockData()
+benchmark.setName(benchmarkName)
+StockData.main(benchmark)
+
+benchmarkReturn = Return()
+Return.mainBenchmark(benchmarkReturn, benchmark)
+
+timeFrame = Return.returnTimeFrame(benchmarkReturn)
+print('Time Frame [years, months]:', timeFrame)
 
 sumOfListLengths = 0
 for i in range(0, numberOfStocks, 1):
-  	print(listOfStocks[i].name)
-  	StockData.main(listOfStocks[i])
+  	print('\n', listOfStocksData[i].name, sep='')
+  	StockData.main(listOfStocksData[i])
   	# Count how many stocks are available
-  	temp = StockData.returnAllLists(listOfStocks[i])
-  	sumOfListLengths = sumOfListLengths + len(temp)
+  	sumOfListLengths = sumOfListLengths + len(StockData.returnAllLists(listOfStocksData[i]))
 
 if sumOfListLengths == 0:
     print("No sources have data for given stocks")
     exit()
 
 # Find return over time using either Jensen's Alpha, Sharpe Ratio, Sortino Ratio, or Treynor Ratio
-#from StockReturn import Return
+for i in range(0, numberOfStocks, 1):
+	print('\n', listOfStocksData[i].name, sep='')
+	#StockReturn.main(listOfStocksReturn[i])
 
 
 # Runs correlation or regression study
-#print(listOfStocks[0].name, listOfStocks[0].absFirstLastDates, listOfStocks[0].finalDatesAndClose)
+# print(listOfStocksData[0].name, listOfStocksData[0].absFirstLastDates, listOfStocksData[0].finalDatesAndClose)
 indicatorFound = False
 while indicatorFound == False:
-	print("\n1. Expense Ratio\n2. Asset Size\n3. Turnover\n4. Persistence\nWhich indicator would you like to look at? ", end='')
-	indicator = str(input())
+	print("1. Expense Ratio\n2. Asset Size\n3. Turnover\n4. Persistence\nWhich indicator would you like to look at? ", end='')
+	
+	#indicator = str(input()) # CHANGE BACK TO THIS LATER
+	indicator = 'Expense Ratio'
+	print(indicator, end='')
+
 	indicatorFound = True
+	print('\n', end='')
 
 	if indicator == 'Expense Ratio' or indicator == '1' or indicator == 'expense ratio':
         #from ExpenseRatio import ExpenseRatio
@@ -66,7 +109,7 @@ while indicatorFound == False:
 
 	else:
 		indicatorFound = False
-		print('\nInvalid input, please enter indicator again')
+		print('Invalid input, please enter indicator again')
 
 '''
 stockName = 'IWV'
